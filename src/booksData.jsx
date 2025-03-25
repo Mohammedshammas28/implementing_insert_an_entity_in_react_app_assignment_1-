aa005a652b2c514d79efa7bc31318ed522a6ecdd
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-03-25 17:19:29 +0530

    Completed the assignment

--- src/booksData.jsx
+++ src/booksData.jsx
@@ -8,7 +8,7 @@ const books = [
     description:
       "A novel about the serious issues of rape and racial inequality, but it is also full of warmth and humor.",
     coverImage:
-      "https://images-na.ssl-images-amazon.com/images/I/81OdwZ9qA9L.jpg",
+      "https://m.media-amazon.com/images/I/71smIoGNeiL._UF1000,1000_QL80_.jpg",
   },
   {
     id: 2,
@@ -29,6 +29,24 @@ const books = [
       "https://images-na.ssl-images-amazon.com/images/I/81af+MCATTL.jpg",
   },
   // Add more books as needed
+  {
+    id:4,
+    title: "Invisible Helix",
+    author: "Keigo Higashino",
+    description:
+     "Detective Galileo, Keigo Higashino's best loved character from The Devotion of Suspect X, returns in a case where hidden history, and impossible crime, are linked by nearly invisible threads in surprising ways.",
+     coverImage:"https://m.media-amazon.com/images/I/813jIgtnF4L._SY425_.jpg"
+  },
+
+  {
+    id:5,
+    title:" Too Good to Be True : A smart, funny will-they-won't-they romance",
+    author:"Prajakta Koli",
+    description:
+      "Avani's favourite thing to do is bury her nose in romance novels, but, honestly, life can't be trusted to make the stories real, right?",
+    coverImage:"https://m.media-amazon.com/images/I/81pGO40uexL._SY466_.jpg",  
+
+  },
 ];
 
 export default books;
